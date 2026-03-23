f27f0c29a6b35bf515588c0bcb2a9d8962cca062
akivagoldstein61-ui <akivagoldstein61@gmail.com>
2026-03-23 14:26:57 +0200

    Initial commit

--- src/services/analyticsService.ts
+++
@@ -1,8 +0,0 @@
-import { AnalyticsEvent } from '../types';
-
-export const analytics = {
-  track(event: AnalyticsEvent, properties?: Record<string, any>) {
-    console.log(`[Analytics] ${event}`, properties || '');
-    // In production, this would send to a real analytics provider
-  }
-};
